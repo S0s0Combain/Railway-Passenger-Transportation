f52aaaf0e4f06fea5f8f570da3afb7f71dc903ca
Artem <kudrinskiy@internet.ru>
2024-05-10 10:15:47 +0300

    Добавьте файлы проекта.

---
+++ Railway Passenger Transportation/Pages/Authorization.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Railway_Passenger_Transportation.Pages
+{
+    public class AuthorizationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Railway Passenger Transportation/Pages/Registration.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Railway_Passenger_Transportation.Pages
+{
+    public class RegistrationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
